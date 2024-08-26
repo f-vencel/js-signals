67ed4e8053bad39922aa5d60f6d5e3240a993df2
f-vencel <148200872+f-vencel@users.noreply.github.com>
2024-08-26 22:48:06 +0200

    added versions to computed and effect signals

--- et/signal2.js
+++ et/signal2.js
@@ -6,26 +6,36 @@ const $computing = Symbol('computing')
 
 const mustRecall = 1
 const mayRecall = 2
-const exsigs = 10
+const inRecall = 10
 
 
 const activeListeners = []
 
-function captureDependency(sig, isSignal) {
-  if (activeListeners.length === 0) return sig.value
-
-  const listener = activeListeners[activeListeners.length - 1]
-
-  if ((listener.checked !== exsigs) || !isSignal) { 
+function pushDependency(listener, sig, isStateSignal) {
+  // if listener is an effect and dependency is a state, then we don't register it, as effects only have computed dependencies
+  if (!((listener.checked === inRecall) && isStateSignal) && !listener.dependencies.includes(sig)) { 
     listener.dependencies.push(sig)
     listener.dependencyValues.push(sig.value)
+    listener.dependencyVersions.push(sig.version)
   }
 
   if (!sig.subscribed.includes(listener)) {
     sig.subscribed.push(listener)
   }
+}
+function captureDependency(sig, isStateSignal) {
+  if (activeListeners.length === 0) return sig.value
+
+  const listener = activeListeners[activeListeners.length - 1]
+
+  pushDependency(listener, sig, isStateSignal)
   
   return sig.value
+}
+function increaseVersion(sig) {
+  sig.version ??= 0
+  sig.version++
+  if (sig.version === Number.MAX_SAFE_INTEGER) sig.version = 0
 }
 
 
@@ -36,25 +46,22 @@ function evaluatePossibleEffects() {
   possibleEffectQueue.forEach(sig => {
     if (sig.checked === mustRecall) return
 
-    if (!allDepsClean(sig)) effectQueue.push(() => sig.call(() => {
-      callBackEffect(sig)
-    }))
+    if (!allDepsClean(sig)) effectQueue.push(effectCallbackFn(sig))
+    else sig.checked = 0
   })
   possibleEffectQueue.length = 0
 }
 function computeEffects() {
-  defaults.defaultrunAllFns(effectQueue)
+  defaults.runEffectsFn(effectQueue)
   effectQueue.length = 0
 }
 
 export const defaults = {
-  defaultEqual,
-  defaultFnCall(def) {
-    return def.async ? defaultAsyncFnCall : defaultFnCall
-  },
-  defaultAsyncFnCall,
+  equal: defaultEqual,
+  syncEffectFn: defaultSyncEffectFn,
+  asyncEffectFn: defaultAsyncEffectFn,
   async: false,
-  defaultrunAllFns: (fnArray) => {
+  runEffectsFn: (fnArray) => {
     fnArray.forEach(fn => fn())
   }
 }
@@ -62,10 +69,10 @@ export const defaults = {
 function defaultEqual(a, b) {
   return Object.is(a, b)
 }
-function defaultFnCall(fn) {
-  fn()
+function defaultSyncEffectFn(fn) {
+  return fn()
 }
-function defaultAsyncFnCall(fn) {
+function defaultAsyncEffectFn(fn) {
   queueMicrotask(fn)
 }
 
@@ -73,18 +80,56 @@ function allDepsClean(sig) {
   for (let i = 0; i < sig.dependencies.length; i++) {
     const dep = sig.dependencies[i]
     
-    if (!dep.equal(dep.value, sig.dependencyValues[i])) return false
-    
+    // if dependency version is different then dependency is not clean
+    if ((dep.version !== sig.dependencyVersions[i]) || !dep.equal(dep.value, sig.dependencyValues[i])) return false
+      
     if (dep.type === 'computed') {
       if (!dep.clean) evaluateComputed(dep)
 
-      if (!dep.equal(dep.value, sig.dependencyValues[i])) return false
+      if ((dep.version !== sig.dependencyVersions[i]) || !dep.equal(dep.value, sig.dependencyValues[i])) return false
     }
   }
 
   return true
 }
+function checkOldDependencies(oldDependencies, sig) {
+  oldDependencies.forEach(dep => {
+    if (!sig.dependencies.includes(dep)) {
+      dep.subscribed.splice(dep.subscribed.indexOf(sig), 1)
+    }
+  })
+}
 
+function setToComputing(sig) {
+  if (sig.value === $computing)
+    throw new Error('cycle in signals\n//? (shouldn\'t be thrown, because this signal was already successfully computed before)')
+  sig.value = $computing
+}
+function callCallBackWithCapture(sig, isEffect) {
+  activeListeners.push(sig)
+  if (isEffect) sig.checked = inRecall
+  
+  const newValue = sig.callback()
+  
+  activeListeners.pop()
+  if (isEffect) sig.checked = 0
+
+  return newValue
+}
+
+function setUpDependencies(sig) {
+  sig.dependencies = []
+  sig.dependencyValues = []
+  sig.dependencyVersions = []
+}
+function setNewDependencies(sig) {
+  const oldDependencies = sig.dependencies
+  sig.dependencies = []
+  sig.dependencyValues.length = 0
+  sig.dependencyVersions.length = 0
+  
+  return oldDependencies
+}
 function evaluateComputed(sig) {
   sig.clean = true
 
@@ -93,77 +138,56 @@ function evaluateComputed(sig) {
   const oldValue = sig.value
   const newValue = callBackComputed(sig)
 
-  if (!sig.equal(oldValue, newValue)) sig.value = newValue
+  if (!sig.equal(oldValue, newValue)) {
+    sig.value = newValue
+    increaseVersion(sig)
+  }
   else sig.value = oldValue
 }
 function callBackComputed(sig) {
-  if (sig.value === $computing)
-    throw new Error('cycle in signals\n//? (shouldn\'t be thrown, because this signal was already successfully computed)')
-  sig.value = $computing
+  setToComputing(sig)
   
-  const oldDependencies = sig.dependencies
-  sig.dependencies = []
-  sig.dependencyValues.length = 0
+  const oldDependencies = setNewDependencies(sig)
 
-  activeListeners.push(sig)
-  
-  const newValue = sig.callback()
-  
-  activeListeners.pop()
+  const newValue = callCallBackWithCapture(sig)
 
-  oldDependencies.forEach(dep => {
-    if (!sig.dependencies.includes(dep)) {
-      dep.subscribed.splice(dep.subscribed.indexOf(sig), 1)
-    }
-  })
+  checkOldDependencies(oldDependencies, sig)
   
   return newValue
 }
 function callBackEffect(sig) {
-  const oldDependencies = sig.dependencies
-  sig.dependencies = []
-  sig.dependencyValues.length = 0
+  const oldDependencies = setNewDependencies(sig)
 
-  activeListeners.push(sig)
-  sig.checked = exsigs
+  const newValue = callCallBackWithCapture(sig, true)
   
-  const newValue = sig.callback()
-  
-  activeListeners.pop()
-  sig.checked = 0
-  
-  oldDependencies.forEach(dep => {
-    if (!sig.dependencies.includes(dep)) {
-      dep.subscribed.splice(dep.subscribed.indexOf(sig), 1)
-    }
-  })
+  checkOldDependencies(oldDependencies, sig)
   
   return newValue
 }
 
-function setUpDependencies(sig) {
-  sig.dependencies = []
-  sig.dependencyValues = []
+function effectCallbackFn(sig) {
+  return () => sig.call(() => callBackEffect(sig))
 }
 
 
+function markEffectDirty(sig, fromState) {
+  if (fromState) {
+    if (sig.checked === mustRecall) return
+
+    sig.checked = mustRecall  // effect certainly needs to be recalled (async)
+    effectQueue.push(effectCallbackFn(sig))
+  }
+  else {
+    if (sig.checked) return
+
+    sig.checked = mayRecall  // effect needs to be checked before recall (async check)
+    possibleEffectQueue.push(sig)
+  }
+}
 function markSubscribedDirty(sig, fromState) {
   sig.subscribed.forEach(sub => {
     if (sub.type === 'effect') {
-      if (fromState) {
-        if (sub.checked === mustRecall) return
-
-        sub.checked = mustRecall  // effect certainly needs to be recalled
-        effectQueue.push(() => sub.call(() => {
-          callBackEffect(sub)
-        }))
-      }
-      else {
-        if (sub.checked) return
-
-        sub.checked = mayRecall  // effect needs to be checked for recall
-        possibleEffectQueue.push(sub)
-      }
+      markEffectDirty(sub, fromState)
       return
     }
     
@@ -183,6 +207,7 @@ function setSignal(sig, value) {
   if (sig.equal(sig.value, value)) return sig.value
 
   sig.value = value
+  increaseVersion(sig)
 
   markSubscribedDirty(sig, true)
 
@@ -196,20 +221,24 @@ function getComputed(sig) {
   if (sig.value === $computing)
     throw new Error('cycle in signals\ncallback function: ' + sig.callback + '\n')
 
-  if (sig.value === $unset) {
-    setUpDependencies(sig)
-
-    sig.value = callBackComputed(sig)
-        
-    return captureDependency(sig)
-  }
+  if (sig.value === $unset) return getUnsetComputed(sig)
 
   if (sig.clean) return captureDependency(sig)
 
   evaluateComputed(sig)
-
   return captureDependency(sig)
 }
+
+function getUnsetComputed(sig) {
+  setUpDependencies(sig)
+
+  sig.value = callBackComputed(sig)
+  increaseVersion(sig)
+      
+  return captureDependency(sig)
+}
+
+
 
 function signalToString(sig) {
   return `[signal ${sig.value}]`
@@ -230,7 +259,7 @@ export function signal(init, options) {
   sig[$sigID] = {
     value: init,
     subscribed: [],
-    equal: options?.equal ?? defaults.defaultEqual
+    equal: options?.equal ?? defaults.equal,
   }
 
   sig.get = () => getSignal(sig[$sigID])
@@ -251,7 +280,7 @@ export function computed(callback, options) {
     callback,
     clean: true,
     subscribed: [],
-    equal: options?.equal ?? defaults.defaultEqual
+    equal: options?.equal ?? defaults.equal
   }
 
   sig.get = () => getComputed(sig[$sigID])
@@ -266,18 +295,17 @@ export function effect(callback, options) {
   effect[$sigID] = {
     type: 'effect',
     callback,
-    equal: options?.equal ?? defaults.defaultEqual,
     call:
       (options?.call === 'async')
-        ? (fn) => defaults.defaultAsyncFnCall(fn)
-        : options?.call ?? ((fn) => defaults.defaultFnCall(defaults)(fn)),
-    dependencies: [],
-    dependencyValues: []
-  }  
+        ? defaults.asyncEffectFn
+        : options?.call ?? (defaults.async ? defaults.asyncEffectFn : defaults.syncEffectFn),
+  }
+  // effect only has computed as dependencies
 
-  effect.run = () => effect[$sigID].call(() => callBackEffect(effect[$sigID]))
+  effect.run = effectCallbackFn(effect[$sigID])
   effect.toString = () => effectToString(effect[$sigID])
 
+  setUpDependencies(effect[$sigID])
   effect.run()
 
   return effect
--- et/test2.js
+++ et/test2.js
@@ -2,24 +2,45 @@ import { signal, computed, effect, defaults } from './signal2.js';
 import * as core from '@angular/core'
 
 
-defaults.async = true
+// defaults.async = true
 
 
-const count = signal(9)
-const isDarkSig = signal(false)
-const isDark = computed(() => isDarkSig())
+// const count = signal(9)
+// const isDarkSig = signal(false)
+// const isDark = computed(() => isDarkSig())
 
-const doubleCount = computed(() => count() * 2)
-const countMod = computed(() => count() % 2)
+// const doubleCount = computed(() => count() * 2)
+// const countMod = computed(() => count() % 2)
 
-const _e =
+// const _e =
+// effect(() => {
+//   console.log('effect')
+//   if (isDark())
+//     console.log(doubleCount())
+//   else
+//   console.log(countMod())
+// })
+
+// isDarkSig(true)
+// isDarkSig(false)
+
+
+const a = signal(0);
+const b = computed(() => {
+  console.log('b')
+  return a() - a()
+})
+b()
+
 effect(() => {
   console.log('effect')
-  if (isDark())
-    console.log(doubleCount())
-  else
-  console.log(countMod())
+  b()
 })
 
-isDarkSig(true)
-isDarkSig(false)
+a(2)
+a(3)
+a(4)
+a(66)
+
+// console.log(b())
+
